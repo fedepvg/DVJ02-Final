1a0ffbbd3c876e3a1f7b0c8be9363112eb77cd7e
fedepvg <federicopvg@gmail.com>
2019-12-09 19:35:53 -0300

    player controller created

---
+++ DVJ02-Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed;
+    public float RotationSpeed;
+    public LayerMask RaycastLayer;
+    public float RaycastDistance;
+
+    Vector3 DestPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(mouseRay, out hit, RaycastDistance, RaycastLayer))
+            {
+                DestPosition = hit.point;
+            }
+        }
+    }
+}
